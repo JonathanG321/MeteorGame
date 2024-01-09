e8bb96dff4cfb59f8e2792cf39f7bdd1920e8de6
JonathanG321 <JonathanG321@ymail.com>
2024-01-09 15:15:05 -0500

    refactor to fix border problems

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -9,15 +9,14 @@ export default function Canvas({
   React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
 >) {
   return (
-    <div
-      id="canvas"
-      {...rest}
-      className={classNames(
-        "h-[30rem] w-[50rem] border-4 border-black",
-        className
-      )}
-    >
-      {children}
+    <div className="border-4 border-black">
+      <div
+        id="canvas"
+        {...rest}
+        className={classNames("h-[30rem] w-[50rem]", className)}
+      >
+        {children}
+      </div>
     </div>
   );
 }
