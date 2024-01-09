3bb819445b5bcc2c37cbaeb20d5c951f969dce24
JonathanG321 <JonathanG321@ymail.com>
2024-01-09 15:30:32 -0500

    add variables file

---
+++ src/variables.ts
@@ -0,0 +1,12 @@
+export const frameRate = 1000 / 60;
+export const screenHeight = 480;
+export const screenWidth = 800;
+export const heroSize = 40;
+export const heroSpeed = 6;
+export const heroJumpHeight = 20;
+export const heroGravity = 2;
+export const maxHeroVelocityDown = 10;
+export const heroSpawnPoint = {
+  X: screenWidth / 2 - heroSize,
+  Y: screenHeight - heroSize,
+};
