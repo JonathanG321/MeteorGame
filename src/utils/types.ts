094407cdcee71c799f5b659b092700b2d40a1cf4
JonathanG321 <JonathanG321@ymail.com>
2024-01-09 18:16:07 -0500

    add types and lib to utils folder

---
+++ src/utils/types.ts
@@ -0,0 +1,11 @@
+export type PressedKeys = {
+  ArrowUp: boolean;
+  ArrowDown: boolean;
+  ArrowLeft: boolean;
+  ArrowRight: boolean;
+};
+
+export type Position = {
+  X: number;
+  Y: number;
+};
