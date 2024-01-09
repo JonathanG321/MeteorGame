692efb24eaf0fa5a39b64b82dd3c35a2703fa70c
JonathanG321 <JonathanG321@ymail.com>
2024-01-09 18:17:07 -0500

    move addEventListeners function to separate file

---
+++ src/behaviors/addEventListeners.ts
@@ -0,0 +1,20 @@
+import { PressedKeys } from "../utils/types";
+
+export default function addEventListeners(
+  setPressedKeys: React.Dispatch<React.SetStateAction<PressedKeys>>
+) {
+  function handleKeyDown(e: KeyboardEvent) {
+    setPressedKeys((prevKeys) => ({ ...prevKeys, [e.key]: true }));
+  }
+  function handleKeyUp(e: KeyboardEvent) {
+    setPressedKeys((prevKeys) => ({ ...prevKeys, [e.key]: false }));
+  }
+
+  document.addEventListener("keydown", handleKeyDown);
+  document.addEventListener("keyup", handleKeyUp);
+
+  return () => {
+    document.removeEventListener("keydown", handleKeyDown);
+    document.removeEventListener("keyup", handleKeyUp);
+  };
+}
