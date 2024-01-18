43b713fba527201944410563c69d868448f32c67
JonathanG321 <JonathanG321@ymail.com>
2024-01-17 22:48:11 -0500

    use heart for health power up

--- src/components/PowerUp.tsx
+++ src/components/PowerUp.tsx
@@ -2,13 +2,17 @@ import classNames from "classnames";
 import { createObjectStyle } from "../utils/lib";
 import { FallingObject } from "../utils/types";
 import { OBJECT_SIZE } from "../utils/variables";
+import heart from "../assets/PixelHeart.png";
 
 export default function PowerUp({ object }: { object: FallingObject }) {
   const style = createObjectStyle(object, OBJECT_SIZE);
   return (
-    <div
+    <img
+      src={object.type === "health" ? heart : ""}
       style={style}
-      className={classNames("absolute bg-purple-500", object.type)}
+      className={classNames("absolute", object.type, {
+        "bg-purple-500": object.type !== "health",
+      })}
     />
   );
 }
