b45643ccacfce79f03f987202b7b7e92dffcb02c
JonathanG321 <JonathanG321@ymail.com>
2024-01-17 22:15:33 -0500

    add usePowerUp hook

---
+++ src/hooks/usePowerUp.ts
@@ -0,0 +1,14 @@
+import { FallingObjectType } from "../utils/types";
+
+export default function usePowerUps(
+  hitObjectType: FallingObjectType | null,
+  setLives: React.Dispatch<React.SetStateAction<number>>,
+  setHitObjectType: React.Dispatch<
+    React.SetStateAction<FallingObjectType | null>
+  >
+) {
+  if (hitObjectType === "health") {
+    setLives((previousLives) => previousLives + 1);
+    setHitObjectType(null);
+  }
+}
