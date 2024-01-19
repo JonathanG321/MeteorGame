356ec0f949ce339cf2af4ec838cfc01d18b77021
JonathanG321 <JonathanG321@ymail.com>
2024-01-19 14:54:08 -0500

    add new types for context refs

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,5 @@
+import { MutableRefObject } from "react";
+
 export type PressedKeys = {
   ArrowUp: boolean;
   ArrowDown: boolean;
@@ -43,3 +45,48 @@ export type FallingObjectOptionsIsCollectable = {
 export type FallingObjectOptions =
   | FallingObjectOptionsNoCollectable
   | FallingObjectOptionsIsCollectable;
+
+export type ObjectWithRefs<T> = {
+  [K in keyof T]: MutableRefObject<T[K]>;
+};
+
+export type ContextValues = {
+  isHit: boolean;
+  powerUpPositions: FallingObject[];
+  setPowerUpPositions: React.Dispatch<React.SetStateAction<FallingObject[]>>;
+  meteorPositions: FallingObject[];
+  setMeteorPositions: React.Dispatch<React.SetStateAction<FallingObject[]>>;
+  lives: number;
+  setLives: React.Dispatch<React.SetStateAction<number>>;
+  setInvincibleCount: React.Dispatch<React.SetStateAction<number>>;
+  invincibleCount: number;
+  setShieldCount: React.Dispatch<React.SetStateAction<number>>;
+  shieldCount: number;
+  mousePressPosition: NullablePosition;
+  setMousePressPosition: React.Dispatch<React.SetStateAction<NullablePosition>>;
+  pressedKeys: PressedKeys;
+  setPressedKeys: React.Dispatch<React.SetStateAction<PressedKeys>>;
+  points: number;
+  setPoints: React.Dispatch<React.SetStateAction<number>>;
+  highScore: number;
+  setHighScore: React.Dispatch<React.SetStateAction<number>>;
+  heroOriginPoint: Position;
+  setHeroOriginPoint: React.Dispatch<React.SetStateAction<Position>>;
+  isGameOver: boolean;
+  setIsGameOver: React.Dispatch<React.SetStateAction<boolean>>;
+  shouldStopGame: boolean;
+  isMainMenu: boolean;
+  setIsMainMenu: React.Dispatch<React.SetStateAction<boolean>>;
+  hitObjectType: FallingObjectType | null;
+  setHitObjectType: React.Dispatch<
+    React.SetStateAction<FallingObjectType | null>
+  >;
+  heroVelocityDown: number;
+  setHeroVelocityDown: React.Dispatch<React.SetStateAction<number>>;
+  slowCount: number;
+  setSlowCount: React.Dispatch<React.SetStateAction<number>>;
+  hero: {
+    position: Position;
+    updatePosition: (partialPosition: Partial<Position>) => void;
+  };
+};
