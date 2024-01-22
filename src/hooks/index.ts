3176105e3a518885d6c1e3630d90360a099882e3
JonathanG321 <JonathanG321@ymail.com>
2024-01-22 14:16:50 -0500

    add hooks index

---
+++ src/hooks/index.ts
@@ -0,0 +1,35 @@
+import useBasicState from "./useBasicState";
+import useClick from "./useClick";
+import useCollectPowerUp from "./useCollectPowerUp";
+import useContextRefsUpdates from "./useContextRefsUpdates";
+import useContextValues from "./useContextValues";
+import useDamageCalculation from "./useDamageCalculation";
+import useDetectCollision from "./useDetectCollision";
+import useFallingObject from "./useObjectGravity";
+import useGameOver from "./useGameOver";
+import useHeroMovementLogicX from "./useHeroMovementLogicX";
+import useHeroMovementLogicY from "./useHeroMovementLogicY";
+import usePowerUps from "./usePowerUps";
+import usePressedKeys from "./usePressedKeys";
+import useUpdatingRefsForObject from "./useUpdatingRefsForObject";
+import useSpawnFallingObject from "./useSpawnFallingObject";
+import useObjectGravity from "./useObjectGravity";
+
+export default {
+  useBasicState,
+  useClick,
+  useCollectPowerUp,
+  useContextRefsUpdates,
+  useDetectCollision,
+  useFallingObject,
+  usePowerUps,
+  useUpdatingRefsForObject,
+  useHeroMovementLogicX,
+  useHeroMovementLogicY,
+  usePressedKeys,
+  useGameOver,
+  useDamageCalculation,
+  useContextValues,
+  useSpawnFallingObject,
+  useObjectGravity,
+};
