3c451ab292da0e29d59580fc2f47a51f8ce3b3a8
JonathanG321 <JonathanG321@ymail.com>
2024-01-23 15:26:03 -0500

    refactor object gravity

--- src/hooks/useObjectGravity.ts
+++ src/hooks/useObjectGravity.ts
@@ -11,23 +11,31 @@ export default function useObjectGravity(
   isSlow: boolean,
   isCollectible = false
 ) {
-  setObjectPositions((oldValue) =>
-    oldValue
-      .map((object) => ({
-        ...object,
-        Y: object.Y + (isSlow ? OBJECT_GRAVITY / 2 : OBJECT_GRAVITY),
-      }))
-      .filter((object) => {
-        const isObjectInBounds = object.Y <= SCREEN_HEIGHT + OBJECT_SIZE;
-        if (isCollectible) {
-          const isHittingHero = !!isObjectCollidingWithHero(
-            object,
-            heroOriginPoint
-          );
-          if (isHittingHero) setHitObjectType(object.type);
-          return isObjectInBounds && !isHittingHero;
-        }
-        return isObjectInBounds;
-      })
-  );
+  setObjectPositions((oldValue) => {
+    const updatedObjects = oldValue.map((object) => ({
+      ...object,
+      Y: object.Y + (isSlow ? OBJECT_GRAVITY / 2 : OBJECT_GRAVITY),
+    }));
+
+    if (!isCollectible) {
+      return updatedObjects.filter(
+        (object) => object.Y <= SCREEN_HEIGHT + OBJECT_SIZE
+      );
+    }
+
+    const filteredObjects = updatedObjects.filter((object) => {
+      const isObjectInBounds = object.Y <= SCREEN_HEIGHT + OBJECT_SIZE;
+
+      if (!isObjectInBounds) return false;
+
+      const isHittingHero = !!isObjectCollidingWithHero(
+        object,
+        heroOriginPoint
+      );
+      if (isHittingHero) setHitObjectType(object.type);
+      return !isHittingHero;
+    });
+
+    return filteredObjects;
+  });
 }
