852a8aeba67b41ca0cf60bfbb9c5577e2b33bef4
JonathanG321 <JonathanG321@ymail.com>
2024-01-23 15:26:48 -0500

    add background image

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from "react";
 import classNames from "classnames";
 import { SCREEN_HEIGHT, SCREEN_WIDTH } from "../utils/variables";
+import castle from "../assets/PixelCastle.png";
 
 export default function Canvas({
   children,
@@ -14,7 +15,14 @@ export default function Canvas({
       <div
         id="canvas"
         {...rest}
-        style={{ height: SCREEN_HEIGHT, width: SCREEN_WIDTH }}
+        style={{
+          height: SCREEN_HEIGHT,
+          width: SCREEN_WIDTH,
+          backgroundImage: `url(${castle})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
         className={classNames("relative", className)}
       >
         {children}
