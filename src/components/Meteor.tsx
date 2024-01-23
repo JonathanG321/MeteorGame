a5afd993081a61b0e47f3137d06a579d83d09870
JonathanG321 <JonathanG321@ymail.com>
2024-01-23 16:09:19 -0500

    implement Meteor graphic

--- src/components/Meteor.tsx
+++ src/components/Meteor.tsx
@@ -1,8 +1,22 @@
 import { createObjectStyle } from "../utils/lib";
 import { Position } from "../utils/types";
 import { OBJECT_SIZE } from "../utils/variables";
+import meteor from "../assets/PixelMeteor.gif";
 
 export default function Meteor({ position }: { position: Position }) {
   const style = createObjectStyle(position, OBJECT_SIZE);
-  return <div style={style} className="meteor absolute bg-red-500" />;
+  return (
+    <div
+      style={{
+        ...style,
+        // backgroundImage: `url(${meteor})`,
+        // backgroundRepeat: "no-repeat",
+        // backgroundPosition: "bottom",
+        // backgroundSize: "cover",
+      }}
+      className="meteor absolute"
+    >
+      <img src={meteor} className="absolute bottom-0" />
+    </div>
+  );
 }
