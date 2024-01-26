a6fe403251a2fcb1c4e58bfe5e6da95122dbc195
JonathanG321 <JonathanG321@ymail.com>
2024-01-26 15:05:39 -0500

    tweak headerBar

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -2,22 +2,25 @@ import { useContext } from "react";
 import { GameStateContext } from "../context/GameStateContext";
 import ScoreDisplay from "./ScoreDisplay";
 import heart from "../assets/images/powerUps/PixelHeart.png";
+import darkHeart from "../assets/images/powerUps/PixelHeartDark.png";
 
 export default function HeaderBar() {
   const { highScore, lives } = useContext(GameStateContext);
   return (
     <div className="bg-green flex h-20 w-full items-center justify-between border-4 border-black">
-      <ScoreDisplay />
-      <div className="flex">
+      <div className="ml-2 flex">
         {Array.from({ length: 3 }).map((_, i) => {
           return (
             <div key={i + heart} className="m-1 h-12 w-12">
-              {i < lives && <img src={heart} width={48} height={48} />}
+              <img src={i < lives ? heart : darkHeart} width={48} height={48} />
             </div>
           );
         })}
       </div>
-      <ScoreDisplay header="High Score" displayPoints={highScore} />
+      <div className="flex">
+        <ScoreDisplay />
+        <ScoreDisplay header="High Score" displayPoints={highScore} />
+      </div>
     </div>
   );
 }
