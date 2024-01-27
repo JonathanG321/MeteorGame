de9dd622b85c3c322f78f70e4f346d19e100e3d2
JonathanG321 <JonathanG321@ymail.com>
2024-01-26 19:14:52 -0500

    fix styling on Score display

--- src/components/ScoreDisplay.tsx
+++ src/components/ScoreDisplay.tsx
@@ -1,18 +1,53 @@
 import { useContext } from "react";
 import { GameStateContext } from "../context/GameStateContext";
+import classNames from "classnames";
 
 type Props = {
   header?: string;
   displayPoints?: number;
+  className?: string;
+  left?: boolean;
+  large?: boolean;
 };
 
-export default function ScoreDisplay({ header, displayPoints }: Props) {
+export default function ScoreDisplay({
+  header,
+  displayPoints,
+  left = false,
+  large = false,
+  className,
+}: Props) {
   const { points } = useContext(GameStateContext);
   return (
-    <div className="flex min-w-36 flex-col items-center justify-center">
+    <div
+      className={classNames(
+        "flex min-w-48 flex-col justify-center",
+        className,
+        { "items-center": !left, "ml-6": left }
+      )}
+    >
       <div className="text-lg font-bold">{header ?? "Score"}</div>
-      <div className="text-2xl font-bold">
-        {(displayPoints ?? points).toLocaleString()}
+      <div
+        className={classNames("font-outline-1 flex font-bold", {
+          "text-3xl": large,
+          "text-2xl": !large,
+        })}
+      >
+        {(displayPoints ?? points)
+          .toLocaleString()
+          .split("")
+          .map((char) => (
+            <div
+              className={classNames("flex justify-center text-center", {
+                "w-[22px]": char !== "," && !large,
+                "w-[26px]": char !== "," && large,
+                "w-2": char === "," && !large,
+                "w-3": char === "," && large,
+              })}
+            >
+              {char}
+            </div>
+          ))}
       </div>
     </div>
   );
