ff7c59d080ade14fe555d230686d16c093b4e9b9
JonathanG321 <JonathanG321@ymail.com>
2024-01-29 12:15:18 -0500

    tweak UI styles

--- src/components/UI.tsx
+++ src/components/UI.tsx
@@ -8,11 +8,11 @@ export default function UI() {
   const { lives, gameStage } = useContext(GameStateContext);
   return (
     <div className="absolute z-10 flex w-full items-center justify-between">
-      <ScoreDisplay large left className="text-white" header="" />
-      <div className="font-outline-1 ml-3 flex h-full items-center text-4xl font-bold text-white">
+      <ScoreDisplay large left className="w-1/3 text-white" header="" />
+      <div className="font-outline-1 ml-3 flex h-full w-1/3 items-center justify-center text-3xl font-bold text-white">
         STAGE {gameStage}
       </div>
-      <div className="mr-3 mt-3 flex items-center">
+      <div className="mr-3 mt-3 flex w-1/3 items-end justify-end">
         {Array.from({ length: 3 }).map((_, i) => {
           return (
             <div key={i + heart} className="m-1 h-12 w-12">
