241c2466122eeaab4145d540fb5ea1ff7aad1f33
JonathanG321 <JonathanG321@ymail.com>
2024-01-31 17:20:50 -0500

    refactor heroMovementLogicX

--- src/logic/heroMovementLogicX.ts
+++ src/logic/heroMovementLogicX.ts
@@ -1,12 +1,57 @@
+import { isValidPosition } from "../utils/lib";
 import {
+  ContextValues,
   Direction,
   NullablePosition,
+  ObjectWithRefs,
   Position,
   StateSetter,
 } from "../utils/types";
-import { HERO_SIZE, HERO_SPEED, SCREEN_WIDTH } from "../utils/variables";
+import { HERO_SPEED, WIDTH_MINUS_HERO } from "../utils/variables";
 
 export default function heroMovementLogicX(
+  contextRefs: ObjectWithRefs<ContextValues>,
+  contextValues: ContextValues
+) {
+  const {
+    heroOriginPoint,
+    pressedKeys,
+    slowCount,
+    lastDirection,
+    heroTwoOriginPoint,
+    lastDirectionTwo,
+    isTwoPlayers,
+  } = contextRefs;
+  const {
+    setHeroOriginPoint,
+    setLastDirection,
+    setHeroTwoOriginPoint,
+    setLastDirectionTwo,
+  } = contextValues;
+
+  if (isValidPosition(heroOriginPoint.current))
+    singleHeroLogicX(
+      heroOriginPoint.current.X,
+      pressedKeys.current.ArrowLeft,
+      pressedKeys.current.ArrowRight,
+      !!slowCount.current,
+      lastDirection.current,
+      setHeroOriginPoint,
+      setLastDirection
+    );
+  if (isTwoPlayers.current && isValidPosition(heroTwoOriginPoint.current))
+    singleHeroLogicX(
+      heroTwoOriginPoint.current.X,
+      pressedKeys.current.a,
+      pressedKeys.current.d,
+      !!slowCount.current,
+      lastDirectionTwo.current,
+      setHeroTwoOriginPoint,
+      setLastDirectionTwo
+    );
+}
+
+function singleHeroLogicX(
   heroPositionX: Position["X"],
   pressedKeyLeft: boolean,
   pressedKeyRight: boolean,
@@ -16,15 +61,16 @@ export default function heroMovementLogicX(
   setLastDirection: StateSetter<Direction>
 ) {
   if (!pressedKeyLeft && !pressedKeyRight) return;
-  let newX = heroPositionX;
-  if (pressedKeyLeft) {
-    newX -= isSlow ? HERO_SPEED / 2 : HERO_SPEED;
-    if (lastDirection === "right") setLastDirection("left");
-  } else if (pressedKeyRight) {
-    newX += isSlow ? HERO_SPEED / 2 : HERO_SPEED;
-    if (lastDirection === "left") setLastDirection("right");
-  }
-  if (newX < 0) newX = 0;
-  if (newX > SCREEN_WIDTH - HERO_SIZE) newX = SCREEN_WIDTH - HERO_SIZE;
+
+  const speed = isSlow ? HERO_SPEED / 2 : HERO_SPEED;
+  const direction = pressedKeyLeft ? "left" : "right";
+
+  if (lastDirection !== direction) setLastDirection(direction);
+
+  let newX = Math.min(
+    WIDTH_MINUS_HERO,
+    Math.max(0, heroPositionX + (pressedKeyLeft ? -speed : speed))
+  );
+
   if (newX !== heroPositionX) updatePosition((prev) => ({ ...prev, X: newX }));
 }
