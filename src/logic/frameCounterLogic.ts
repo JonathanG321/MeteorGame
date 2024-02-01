3573906318bd840029633704ff098a94e123c433
JonathanG321 <JonathanG321@ymail.com>
2024-02-01 13:19:53 -0500

    refactor frameCounterLogic

--- src/logic/frameCounterLogic.ts
+++ src/logic/frameCounterLogic.ts
@@ -1,4 +1,4 @@
-import { countDownTo0, getPowerUpList } from "../utils/lib";
+import { countDownTo0 } from "../utils/lib";
 import { ContextValues, ObjectWithRefs } from "../utils/types";
 import { STAGE_DIFFICULTY_SCALE } from "../utils/variables";
 
@@ -10,23 +10,23 @@ export default function frameCounterLogic(
   const { setSlowCount, setPoints, setPointsTwo, setGameCounter } =
     contextValues;
 
-  const currentSlowCount = slowCount.current;
-  const slowCountEffect = currentSlowCount > 0 ? 20 : 10;
+  const isSlow = slowCount.current > 0;
+  const slowCountEffect = isSlow ? 20 : 10;
 
-  setSlowCount((prevValue) => countDownTo0(prevValue, !!currentSlowCount));
+  setSlowCount((prevValue) => countDownTo0(prevValue, isSlow));
 
   if (lives.current !== 0)
     setPoints((prevValue) => prevValue + slowCountEffect);
   if (isTwoPlayers && livesTwo.current !== 0)
     setPointsTwo((prevValue) => prevValue + slowCountEffect);
 
-  setGameCounter((prevValue) => prevValue + (currentSlowCount > 0 ? 1 : 2));
+  setGameCounter((prevValue) => prevValue + (isSlow ? 1 : 2));
 
   const gameStageMultiplier =
     STAGE_DIFFICULTY_SCALE ** (gameStage.current <= 5 ? gameStage.current : 5);
 
   return {
-    slowCount: currentSlowCount,
+    slowCount: slowCount.current,
     gameStage: gameStage.current,
     gameStageMultiplier,
   };
