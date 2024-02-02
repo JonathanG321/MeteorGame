5e4c5e6b29db9e684fbcc5e9901923fbbad5f277
JonathanG321 <JonathanG321@ymail.com>
2024-02-02 16:15:35 -0500

    add special meteor logic

---
+++ src/logic/specialMeteorLogic.ts
@@ -0,0 +1,39 @@
+import { randomInRange } from "../utils/lib";
+import { ContextValues, FallingObject, ObjectWithRefs } from "../utils/types";
+import {
+  SPECIAL_METEOR_SIZE,
+  SPECIAL_METEOR_SPAWN_RANGE,
+  SPECIAL_METEOR_EXPLOSION_CHANCE,
+} from "../utils/variables";
+
+export default function specialMeteorLogic(
+  contextRefs: ObjectWithRefs<ContextValues>,
+  contextValues: ContextValues
+) {
+  const { specialPositions } = contextRefs;
+  const { setMeteorPositions, setSpecialPositions } = contextValues;
+  specialPositions.current.forEach((meteor) => {
+    if (Math.random() * 100 < SPECIAL_METEOR_EXPLOSION_CHANCE) {
+      setSpecialPositions((prev) =>
+        prev.filter((object) => object.id !== meteor.id)
+      );
+      setMeteorPositions((prev) => [...prev, ...createThreeNewMeteors(meteor)]);
+    }
+  });
+}
+
+function createThreeNewMeteors(specialMeteor: FallingObject) {
+  const newSize = SPECIAL_METEOR_SIZE / 2;
+  const minPosition = specialMeteor.X - SPECIAL_METEOR_SPAWN_RANGE;
+  const maxPosition = specialMeteor.X + SPECIAL_METEOR_SPAWN_RANGE;
+  return Array.from({ length: 3 }).map(
+    (): FallingObject => ({
+      Y: specialMeteor.Y,
+      X: randomInRange(minPosition, maxPosition),
+      id: crypto.randomUUID(),
+      type: "meteor",
+      size: newSize,
+      speed: specialMeteor.speed,
+    })
+  );
+}
