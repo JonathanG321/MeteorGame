fa182c7569812db78b9a8a816d8a01be5f135732
JonathanG321 <JonathanG321@ymail.com>
2024-02-03 02:14:10 -0500

    add notEmpty helper function

--- src/utils/lib.ts
+++ src/utils/lib.ts
@@ -115,3 +115,7 @@ export function calcIsHit(
     ? !!detectCollision(meteorPositions, heroOriginPoint)
     : false;
 }
+
+export function notEmpty<TValue>(value: TValue | null | undefined): value is TValue {
+  return value !== null && value !== undefined;
+}
