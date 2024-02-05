96355640309dafe8043625c7f0a95f5b7582b18c
JonathanG321 <JonathanG321@ymail.com>
2024-02-04 21:25:34 -0500

    refactor frameCounterLogic

--- src/logic/frameCounterLogic.ts
+++ src/logic/frameCounterLogic.ts
@@ -1,5 +1,10 @@
 import { countDownTo0, isValidPosition, setPlayerValue } from "../utils/lib";
-import { ContextValues, ObjectWithRefs } from "../utils/types";
+import {
+  ContextValues,
+  NullablePlayer,
+  ObjectWithRefs,
+  StateSetter,
+} from "../utils/types";
 import { STAGE_DIFFICULTY_SCALE } from "../utils/variables";
 
 export default function frameCounterLogic(
@@ -14,17 +19,9 @@ export default function frameCounterLogic(
 
   setSlowCount((prev) => countDownTo0(prev, isSlow));
 
-  function updatePoints(index: number) {
-    setPlayers((prev) =>
-      setPlayerValue(prev, index, {
-        points: prev[index].points + slowCountEffect,
-      })
-    );
-  }
-
-  players.current.forEach((player, i) => {
-    if (isValidPosition(player)) updatePoints(i);
-  });
+  players.current.forEach((player, index) =>
+    processPlayer(player, index, slowCountEffect, setPlayers)
+  );
 
   setGameCounter((prev) => prev + (isSlow ? 1 : 2));
 
@@ -36,4 +33,27 @@ export default function frameCounterLogic(
     gameStage: gameStage.current,
     gameStageMultiplier,
   };
+}
+
+function updatePlayerPoints(
+  playerIndex: number,
+  slowCountEffect: number,
+  setPlayers: StateSetter<NullablePlayer[]>
+) {
+  setPlayers((prevPlayers) =>
+    setPlayerValue(prevPlayers, playerIndex, {
+      points: prevPlayers[playerIndex].points + slowCountEffect,
+    })
+  );
+}
+
+function processPlayer(
+  player: NullablePlayer,
+  index: number,
+  slowCountEffect: number,
+  setPlayers: StateSetter<NullablePlayer[]>
+) {
+  if (isValidPosition(player)) {
+    updatePlayerPoints(index, slowCountEffect, setPlayers);
+  }
 }
