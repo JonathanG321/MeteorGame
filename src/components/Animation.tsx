e966ff6e064803dceb741383f8810691a7a9c07d
JonathanG321 <JonathanG321@ymail.com>
2024-02-05 13:57:40 -0500

    add Animation component

---
+++ src/components/Animation.tsx
@@ -0,0 +1,53 @@
+import classNames from "classnames";
+import { Animation } from "../utils/types";
+import pointsSmall from "../assets/images/powerUps/PixelCoin.gif";
+import explosion from "../assets/images/PixelExplosion.gif";
+import { useContext, useEffect, useState } from "react";
+import { GameStateContext } from "../context/GameStateContext";
+import { createObjectStyle } from "../utils/lib";
+import { POINTS_ANIMATION_DURATION } from "../utils/variables";
+
+type Props = {
+  animation: Animation;
+};
+
+export default function FallingObject({ animation }: Props) {
+  const { setAnimationPositions } = useContext(GameStateContext);
+  const [offset, setOffset] = useState(0);
+  const style = createObjectStyle({ ...animation, Y: animation.Y - offset });
+  let points: number | null = null;
+
+  if (animation.type === "points") points = animation.points;
+
+  useEffect(() => {
+    const intervalId = setInterval(() => setOffset((prev) => prev + 1), 40);
+    setTimeout(() => {
+      setAnimationPositions((prev) =>
+        prev.filter((ani) => ani.id !== animation.id)
+      );
+    }, POINTS_ANIMATION_DURATION);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <div
+      style={style}
+      className={classNames(
+        "font-outline-1 absolute flex text-white",
+        animation.type
+      )}
+    >
+      {animation.type === "points" && (
+        <>
+          <div>+{animation.points}</div>
+          <img src={pointsSmall} className="mt-[5px] h-[17px] w-[17px]" />
+        </>
+      )}
+      {animation.type === "explosion" && (
+        <img src={explosion} className="absolute bottom-0 left-0" />
+      )}
+    </div>
+  );
+}
