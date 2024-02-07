964c62708e607fe5240d050c4f111c0b9d29006b
JonathanG321 <JonathanG321@ymail.com>
2024-02-07 12:37:14 -0500

    change background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import MeteorHero from "./components/MeteorHero";
 import { GAME_HEIGHT, SCREEN_WIDTH } from "./utils/variables";
-import parchment from "./assets/images/PixelParchment.png";
+import bricks from "./assets/images/PixelBricks.jpeg";
 
 export default function App() {
   return (
     <div
-      style={{ backgroundImage: `url(${parchment})` }}
+      style={{ backgroundImage: `url(${bricks})` }}
       className="flex h-screen w-full items-center justify-center bg-cover bg-center bg-no-repeat"
     >
       <MeteorHero height={GAME_HEIGHT} width={SCREEN_WIDTH} />
