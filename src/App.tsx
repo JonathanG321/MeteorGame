0c2cb5b8c76344b3cedf174ba9cfa0228d16a433
JonathanG321 <JonathanG321@ymail.com>
2024-02-06 20:50:23 -0500

    restyle main App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import MeteorHero from "./components/MeteorHero";
 import { SCREEN_HEIGHT, SCREEN_WIDTH } from "./utils/variables";
+import parchment from "./assets/images/PixelParchment.png";
 
 export default function App() {
-  return <MeteorHero height={SCREEN_HEIGHT} width={SCREEN_WIDTH} />;
+  return (
+    <div
+      style={{ backgroundImage: `url(${parchment})` }}
+      className="flex h-screen w-full items-center justify-center bg-cover bg-center bg-no-repeat"
+    >
+      <MeteorHero height={SCREEN_HEIGHT} width={SCREEN_WIDTH} />
+    </div>
+  );
 }
