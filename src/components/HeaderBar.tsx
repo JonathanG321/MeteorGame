473e2d240ce83913f7450e06c8a959248bf3deac
JonathanG321 <JonathanG321@ymail.com>
2024-02-08 22:33:31 -0500

    fix incorrect header scaling

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -3,6 +3,7 @@ import parchment from "../assets/images/PixelParchment.png";
 import { useContext } from "react";
 import { GameStateContext } from "../context/GameStateContext";
 import { getFontSize } from "../utils/lib";
+import { BORDER_WIDTH } from "../utils/variables";
 
 type Props = { highScore: number; width: number; height: number };
 
@@ -10,10 +11,22 @@ export default function HeaderBar({ highScore, width, height }: Props) {
   const { scale } = useContext(GameStateContext);
   return (
     <div
-      style={{ width, height, backgroundImage: `url(${parchment})` }}
-      className="font-outline-1 flex w-full items-center justify-between border-4 border-black bg-cover bg-center bg-no-repeat text-white"
+      style={{
+        width,
+        height,
+        backgroundImage: `url(${parchment})`,
+        borderWidth: `${BORDER_WIDTH * scale}px`,
+        WebkitTextStroke: `${1 * scale}px black`,
+      }}
+      className="flex w-full items-center justify-between border-black bg-cover bg-center bg-no-repeat text-white"
     >
-      <h1 style={getFontSize("4xl", scale)} className="font-outline-2 ml-4">
+      <h1
+        style={{
+          ...getFontSize("4xl", scale),
+          WebkitTextStroke: `${2 * scale}px black`,
+          marginLeft: `${16 * scale}px`,
+        }}
+      >
         Meteor Hero
       </h1>
       <ScoreDisplay header="High Score" displayPoints={highScore} />
