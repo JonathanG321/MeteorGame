25e6739b5aa0d302641114c09bc2704fa591da19
JonathanG321 <JonathanG321@ymail.com>
2024-02-08 23:12:41 -0500

    fix border and font scaling in menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,17 +2,23 @@ import { useContext } from "react";
 import { GameStateContext } from "../context/GameStateContext";
 import GameOverScreen from "./GameOverScreen";
 import MenuScreen from "./MenuScreen";
+import { BORDER_WIDTH } from "../utils/variables";
 
 export default function Menu() {
-  const { isGameOver, isMainMenu, screenHeight, screenWidth } =
+  const { isGameOver, isMainMenu, screenHeight, screenWidth, scale } =
     useContext(GameStateContext);
 
   return (
     <div className="absolute z-20 flex h-full w-full items-center justify-center">
       <div className="absolute z-20 h-full w-full bg-gray-500 opacity-30" />
       <div
-        style={{ width: screenWidth / 2, height: screenHeight / 2 }}
-        className="z-20 flex flex-col items-center justify-center border-4 border-black bg-white p-5"
+        style={{
+          width: screenWidth / 2,
+          height: screenHeight / 2,
+          borderWidth: `${BORDER_WIDTH * scale}px`,
+          padding: `${20 * scale}px`,
+        }}
+        className="z-20 flex flex-col items-center justify-center border-black bg-white"
       >
         {isGameOver && <GameOverScreen />}
         {isMainMenu && <MenuScreen />}
