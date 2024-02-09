867f17894f125c2f8fedd1e2202aaa9da06a30af
JonathanG321 <JonathanG321@ymail.com>
2024-02-08 22:32:17 -0500

    fix canvas border scaling

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,19 +1,22 @@
 import { PropsWithChildren } from "react";
 import classNames from "classnames";
 import castle from "../assets/images/PixelCastle.png";
+import { BORDER_WIDTH } from "../utils/variables";
 
 export default function Canvas({
   children,
   className,
   height,
   width,
+  scale,
   ...rest
 }: PropsWithChildren<
   React.DetailedHTMLProps<
     React.HTMLAttributes<HTMLDivElement>,
     HTMLDivElement
-  > & { height: number; width: number }
+  > & { height: number; width: number; scale: number }
 >) {
+  const borderStyle = `${BORDER_WIDTH * scale}px`;
   return (
     <div
       id="canvas"
@@ -21,10 +24,14 @@ export default function Canvas({
       style={{
         height,
         width,
+        borderLeftWidth: borderStyle,
+        borderRightWidth: borderStyle,
+        borderBottomWidth: borderStyle,
+        borderTop: 0,
         backgroundImage: `url(${castle})`,
       }}
       className={classNames(
-        `relative overflow-hidden border-x-4 border-b-4 border-black bg-cover bg-center bg-no-repeat`,
+        `relative overflow-hidden border-black bg-cover bg-center bg-no-repeat`,
         className
       )}
     >
